3a6daea28818da68359b646dd20672d4ee3678e4
Tomasz Borkowski <tomasz.borkowski@opuscapita.com>
2024-12-25 16:54:36 +0100

    added to tests branch

--- edi-find.ps1
+++ edi-find.ps1
@@ -15,16 +15,35 @@ $date = Get-Date -Format "yyyy-MM-dd"
 $logFile = "$PSScriptRoot\log_$date.txt"
 
 # define keywords to search for
-$keywords = @("UNH", "BGM");
+$keywords = @("AA", "GH");
+$loop_count = 0
 
 # loop through each file and check for keywords
 foreach ($file in $files) {
-    "File: $($file)" | Out-File -FilePath $logFile -Append
+
+    # replace ' with newline
+    $temp0 = Get-Content $file -Raw
+    $temp = $temp0 -replace "'", [Environment]::NewLine
+    $loop_count
+    $loop_count ++
+    Write-Output Get-Content $temp
+    $file
+
+    foreach ($keyword in $keywords) {
+        $results = Get-Content $temp | Select-String -Pattern $keyword
+        Write-Output $results
+    }
+
+    }
+
+
+
+
+<#     "File: $($file)" | Out-File -FilePath $logFile -Append
     foreach ($keyword in $keywords) {
         $results = Get-Content $file | Select-String -Pattern $keyword
         if ($results) {
             $results | Out-File -FilePath $logFile -Append
         }
     }
-    "----------" | Out-File -FilePath $logFile -Append
-}
+    "----------" | Out-File -FilePath $logFile -Append #>
