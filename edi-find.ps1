7162b5ca30222328fd6611f42f3d5257e6567def
Tomasz Borkowski <tomasz.borkowski@opuscapita.com>
2024-12-26 13:23:23 +0100

    merged edi-find.ps1 from tests branch

--- edi-find.ps1
+++ edi-find.ps1
@@ -1,30 +1,82 @@
-# Create list of files to check based on args given by user
-param([Parameter(ValueFromRemainingArguments=$true)]$args)
-$files = @()
-
-if ($args.Count -eq 0) {
-    $files = Get-ChildItem -Path $PSScriptRoot -File | Select-Object -ExpandProperty FullName
-}
-
-else {
-    $files = $args
-}
-
 # log file settings
-$date = Get-Date -Format "yyyy-MM-dd"
-$logFile = "$PSScriptRoot\log_$date.txt"
+#$logFile = "$PSScriptRoot\log_$(Get-Date -Format "yyyy-MM-dd").txt" # save log in script's location
+$logFile = "$(Get-Location)\log_$(Get-Date -Format "yyyy-MM-dd").txt" # save log in current dir
 
 # define keywords to search for
-$keywords = @("UNH", "BGM");
+$keywords = @("UNB+", "AB", "GH", "UNZ+");
+$extensions = @(".txt", ".edi")
 
-# loop through each file and check for keywords
-foreach ($file in $files) {
-    "File: $($file)" | Out-File -FilePath $logFile -Append
-    foreach ($keyword in $keywords) {
-        $results = Get-Content $file | Select-String -Pattern $keyword
-        if ($results) {
-            $results | Out-File -FilePath $logFile -Append
+# basic logic for this script
+function main {
+
+    $files = GetFileList @args
+    
+    foreach ($file in $files) {
+        if (ValidateFile $file) {
+            CreateLogEntry $file
         }
     }
-    "----------" | Out-File -FilePath $logFile -Append
 }
+
+# Create list of files to check based on args given by user
+function GetFileList {
+
+    $list = @()
+
+    if ($args.Count -eq 0) {
+        $list = Get-ChildItem -Path (Get-Location) -File | Select-Object -ExpandProperty FullName
+    }
+    else {
+        $list = $args
+    }  
+
+    return $list
+}
+
+function ValidateFile {
+
+    if (-not (Test-Path $file)) {
+        Write-Host "$($file): file doesn't exist"
+        return $false
+    }
+
+    if (-not ($([System.IO.Path]::GetExtension($file)) -in $extensions)) {
+        Write-Host "$($file): incorrect file extension"
+        return $false
+    }
+
+    if (-not (Select-String $file -Pattern "UNB+" -Quiet)) {
+        Write-Host "$($file): wrong format"
+        return $false
+    }
+
+    return $true
+
+}
+
+function CreateLogEntry {
+
+    $log_entry = @("File: $file", "***")
+
+    $content = (Get-Content $file -Raw) -split "'"
+
+    foreach ($keyword in $keywords) {
+        $results = $content | Select-String -Pattern $keyword
+        if ($results){
+            # check for exceptions
+            if ($keyword -eq "UNB+") {
+                $log_entry += "$results`n-----"
+            }
+            elseif ($keyword -eq "UNZ+") {
+                $log_entry += "-----`n$results`n***`n"
+            } 
+            else {
+                $log_entry += "$results"
+            }                
+        }
+    }
+        $log_entry | Out-File -FilePath $logFile -Append
+        return
+}
+
+main @args
